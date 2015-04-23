d1363606dbb6275a7e2940325e597172db25f768
Josh Williams <jodawill@iuk.edu>
2015-04-23 10:27:13 -0400

    Added verbose mode to allow piping

--- hash37.c
+++ hash37.c
@@ -1,6 +1,8 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define UNSET		-2
 #define ERROR		-1
 #define NO_EXIT		 0
 #define EXIT		 1
@@ -11,32 +13,53 @@
 char letters[] = "acdegilmnoprstuw";
 char word[32];
 long long int hash_input = 945924806726376;
+bool verbose = false;
 
 int check_args(int argc,char *argv[]) {
  if (argc <= 1) return 0;
- if (strcmp(argv[1],"--brute-force") == 0) {
-  if (argc >= 3) {
-   long long int n = atoll(argv[2]);
-   if (n > 0) hash_input = n;
+ int mode = UNSET;
+ for (int i = 1; i < argc; i++) {
+  if (strcmp(argv[i],"--brute-force") == 0) {
+   if (mode != UNSET) goto LABERR;
+   if (argc >= i+2) {
+    long long int n = atoll(argv[++i]);
+    if (n > 0) hash_input = n;
+   }
+   mode = BRUTE_FORCE;
+   continue;
   }
-  return BRUTE_FORCE;
- }
- if (strcmp(argv[1],"--display-hash") == 0) {
-  if (argc >= 3) {
-   strcpy(word,argv[2]);
-   return DISPLAY_HASH;
+  if (strcmp(argv[i],"--display-hash") == 0) {
+   if (mode != UNSET) goto LABERR;
+   if (argc > i+1) {
+    strcpy(word,argv[++i]);
+    mode = DISPLAY_HASH;
+    continue;
+   }
+   printf("Missing required word to translate.\n");
+   return ERROR;
   }
-  printf("Missing required word to translate.\n");
+  if (strcmp(argv[i],"--unravel-hash") == 0) {
+   if (mode != UNSET) goto LABERR;
+   goto LABERR;
+   if (argc > i+1) {
+    long long int n = atoll(argv[++i]);
+    if (n > 0) hash_input = n;
+   }
+   mode = UNRAVEL_HASH;
+  }
+  if (strcmp(argv[i],"--verbose") == 0 || strcmp(argv[i],"-v") == 0) {
+   verbose = true;
+   continue;
+  }
+  printf("Invalid command: %s\n",argv[i]);
   return ERROR;
  }
- if (strcmp(argv[1],"--unravel-hash") == 0) {
-  if (argc >= 3) {
-   long long int n = atoll(argv[2]);
-   if (n > 0) hash_input = n;
-  }
-  return UNRAVEL_HASH;
+ return mode;
+
+ LABERR: {
+  printf("Error: Only one mode may be specified.\n");
+  return ERROR;
  }
- return ERROR;
 }
 
 long long int hash(char *s) {
@@ -54,7 +77,6 @@ int check(char word[], int pos) {
  for (int i = 0; i < strlen(letters); i++) {
   word[pos] = letters[i];
   if (hash(word) == hash_input) {
-   printf("The magic word is: %s\n",word);
    return EXIT;
   }
   if (pos > 0)
@@ -81,19 +103,37 @@ int unravel(long long int h) {
 int main(int argc, char *argv[]) {
  switch (check_args(argc,argv)) {
   case BRUTE_FORCE: {
-   printf("Brute-forcing a result. This might take a while.\n");
+   if (verbose) {
+    printf("Brute-forcing a result. This might take a while.\n");
+   }
    char str[] = "aaaaaaaaa";
    check(str,strlen(str)-1);
+   if (verbose) {
+    printf("The magic word is: %s\n",word);
+   } else {
+    printf("%s\n",word);
+   }
    return 0;
   }
   case DISPLAY_HASH: {
-   printf("The hash for '%s' is: %lld\n",word,hash(word));
+   if (verbose) {
+    printf("The hash for '%s' is: %lld\n",word,hash(word));
+   } else {
+    printf("%lld\n",hash(word));
+   }
    return 0;
   }
   case UNRAVEL_HASH: {
    unravel(hash_input);
-   printf("%s\n",word);
+   if (verbose) {
+    printf("The magic word is: %s\n",word);
+   } else {
+    printf("%s\n",word);
+   }
    return 0;
+  }
+  case ERROR: {
+   return ERROR;
   }
   default: {
    printf("Specify a command: --brute-force, --display-hash WORD, or "
